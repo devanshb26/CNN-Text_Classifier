36c3833ff333122044c6a038277880d3d8d833e4
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-28 00:28:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ LABEL = data.LabelField(dtype = torch.float)
 fields = [(None, None), ('text', TEXT),('label', LABEL)]
 train_data, valid_data, test_data = data.TabularDataset.splits(
                                         path = '',
-                                        train = 'V1.4_Training.csv',
-                                        validation = 'SubtaskA_EvaluationData_labeled.csv',
-                                        test = 'SubtaskA_EvaluationData_labeled.csv',
+#                                         train = 'V1.4_Training.csv',
+#                                         validation = 'SubtaskA_EvaluationData_labeled.csv',
+#                                         test = 'SubtaskA_EvaluationData_labeled.csv',
+                                        train = 'train.tsv',
+                                        validation = 'valid.tsv',
+                                        test = 'test.tsv',
                                         #sort_key=lambda x: len(x.Text),
-                                        format = 'csv',
+                                        format = 'tsv',
                                         fields = fields,
                                         skip_header = False
 )
