753a16aa40dc530f9943f728ba03b423ac106998
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-28 00:30:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ train_data, valid_data, test_data = data.TabularDataset.splits(
                                         #sort_key=lambda x: len(x.Text),
                                         format = 'tsv',
                                         fields = fields,
-                                        skip_header = False
+                                        skip_header = True
 )
 print(vars(train_data[0]))
 MAX_VOCAB_SIZE = 25_000
