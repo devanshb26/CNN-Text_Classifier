88d3b3cfaf593e03396d993198e0fef30c54e1d0
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-27 20:24:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from torchtext import data
 from torchtext import datasets
 
-TEXT = data.Field()
-LABEL = data.Field()
+TEXT = data.Field(tokenize='spacy)
+LABEL = data.LabelField(dtype = torch.float)
 
 fields = [(None, None), (None, None), ('l', LABEL), ('t', TEXT)]
 train_data, valid_data, test_data = data.TabularDataset.splits(
@@ -15,3 +15,88 @@ train_data, valid_data, test_data = data.TabularDataset.splits(
                                         skip_header = False
 )
 print(vars(train_data[0]))
+MAX_VOCAB_SIZE = 25_000
+
+TEXT.build_vocab(train_data, 
+                 max_size = MAX_VOCAB_SIZE, 
+                 vectors = "glove.6B.100d", 
+                 unk_init = torch.Tensor.normal_)
+
+LABEL.build_vocab(train_data)
+                  
+BATCH_SIZE = 64
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data), 
+    batch_size = BATCH_SIZE, 
+    device = device)
+                  
+                  
+                  
+                  
+class CNN(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, 
+                 dropout, pad_idx):
+        
+        super().__init__()
+                
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx = pad_idx)
+        
+        self.convs = nn.ModuleList([
+                                    nn.Conv2d(in_channels = 1, 
+                                              out_channels = n_filters, 
+                                              kernel_size = (fs, embedding_dim)) 
+                                    for fs in filter_sizes
+                                    ])
+        
+        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
+        
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, text):
+        
+        #text = [sent len, batch size]
+        
+        text = text.permute(1, 0)
+                
+        #text = [batch size, sent len]
+        
+        embedded = self.embedding(text)
+                
+        #embedded = [batch size, sent len, emb dim]
+        
+        embedded = embedded.unsqueeze(1)
+        
+        #embedded = [batch size, 1, sent len, emb dim]
+        
+        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
+            
+        #conved_n = [batch size, n_filters, sent len - filter_sizes[n] + 1]
+                
+        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
+        
+        #pooled_n = [batch size, n_filters]
+        
+        cat = self.dropout(torch.cat(pooled, dim = 1))
+
+        #cat = [batch size, n_filters * len(filter_sizes)]
+            
+        return self.fc(cat)
+                 
+INPUT_DIM = len(TEXT.vocab)
+EMBEDDING_DIM = 100
+N_FILTERS = 100
+FILTER_SIZES = [3,4,5]
+OUTPUT_DIM = 1
+DROPOUT = 0.5
+PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
+
+model = CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT, PAD_IDX)
+                  
+                  
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
