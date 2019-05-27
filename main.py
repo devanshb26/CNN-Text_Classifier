cfd6af07946a471d34ecbb8b058841ff1c63cb3a
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-27 20:50:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -142,92 +142,92 @@ def binary_accuracy(preds, y):
     return acc
   
   
-  def train(model, iterator, optimizer, criterion):
-    
-    epoch_loss = 0
-    epoch_acc = 0
-    
-    model.train()
-    
-    for batch in iterator:
-        
-        optimizer.zero_grad()
-        
-        predictions = model(batch.text).squeeze(1)
-        
-        loss = criterion(predictions, batch.label)
-        
-        acc = binary_accuracy(predictions, batch.label)
-        
-        loss.backward()
-        
-        optimizer.step()
-        
-        epoch_loss += loss.item()
-        epoch_acc += acc.item()
-        
-    return epoch_loss / len(iterator), epoch_acc / len(iterator)
-  
-  
-  
-  def evaluate(model, iterator, criterion):
-    
-    epoch_loss = 0
-    epoch_acc = 0
-    
-    model.eval()
-    
-    with torch.no_grad():
-    
-        for batch in iterator:
+def train(model, iterator, optimizer, criterion):
 
-            predictions = model(batch.text).squeeze(1)
-            
-            loss = criterion(predictions, batch.label)
-            
-            acc = binary_accuracy(predictions, batch.label)
+  epoch_loss = 0
+  epoch_acc = 0
 
-            epoch_loss += loss.item()
-            epoch_acc += acc.item()
-        
-    return epoch_loss / len(iterator), epoch_acc / len(iterator)
-  
-  
-  
+  model.train()
+
+  for batch in iterator:
+
+      optimizer.zero_grad()
+
+      predictions = model(batch.text).squeeze(1)
+
+      loss = criterion(predictions, batch.label)
+
+      acc = binary_accuracy(predictions, batch.label)
+
+      loss.backward()
+
+      optimizer.step()
+
+      epoch_loss += loss.item()
+      epoch_acc += acc.item()
+
+  return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+
+def evaluate(model, iterator, criterion):
+
+  epoch_loss = 0
+  epoch_acc = 0
+
+  model.eval()
+
+  with torch.no_grad():
+
+      for batch in iterator:
+
+          predictions = model(batch.text).squeeze(1)
+
+          loss = criterion(predictions, batch.label)
+
+          acc = binary_accuracy(predictions, batch.label)
+
+          epoch_loss += loss.item()
+          epoch_acc += acc.item()
+
+  return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+
 import time
 
 def epoch_time(start_time, end_time):
-    elapsed_time = end_time - start_time
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
-  
-  
+  elapsed_time = end_time - start_time
+  elapsed_mins = int(elapsed_time / 60)
+  elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+  return elapsed_mins, elapsed_secs
+
+
 
 best_valid_loss = float('inf')
 
 for epoch in range(N_EPOCHS):
 
-    start_time = time.time()
-    
-    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
-    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
-    
-    end_time = time.time()
+  start_time = time.time()
 
-    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
-    
-    if valid_loss < best_valid_loss:
-        best_valid_loss = valid_loss
-        torch.save(model.state_dict(), 'tut4-model.pt')
-    
-    print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
-    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
-    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
-    
-    
-    
-    model.load_state_dict(torch.load('tut4-model.pt'))
+  train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+  valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+
+  end_time = time.time()
+
+  epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+  if valid_loss < best_valid_loss:
+      best_valid_loss = valid_loss
+      torch.save(model.state_dict(), 'tut4-model.pt')
+
+  print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+  print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
+  print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
+
+
+
+  model.load_state_dict(torch.load('tut4-model.pt'))
 
 test_loss, test_acc = evaluate(model, test_iterator, criterion)
 
@@ -238,14 +238,14 @@ import spacy
 nlp = spacy.load('en')
 
 def predict_sentiment(model, sentence, min_len = 5):
-    model.eval()
-    tokenized = [tok.text for tok in nlp.tokenizer(sentence)]
-    if len(tokenized) < min_len:
-        tokenized += ['<pad>'] * (min_len - len(tokenized))
-    indexed = [TEXT.vocab.stoi[t] for t in tokenized]
-    tensor = torch.LongTensor(indexed).to(device)
-    tensor = tensor.unsqueeze(1)
-    prediction = torch.sigmoid(model(tensor))
-    return prediction.item()
-  
+  model.eval()
+  tokenized = [tok.text for tok in nlp.tokenizer(sentence)]
+  if len(tokenized) < min_len:
+      tokenized += ['<pad>'] * (min_len - len(tokenized))
+  indexed = [TEXT.vocab.stoi[t] for t in tokenized]
+  tensor = torch.LongTensor(indexed).to(device)
+  tensor = tensor.unsqueeze(1)
+  prediction = torch.sigmoid(model(tensor))
+  return prediction.item()
+
 
