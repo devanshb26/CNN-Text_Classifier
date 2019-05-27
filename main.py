03c92b1c236d8b3335c51ffefa2d90b818977a96
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-27 21:23:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -202,8 +202,7 @@ def epoch_time(start_time, end_time):
   elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
   return elapsed_mins, elapsed_secs
 
-
-
+N_EPOCHS = 5
 best_valid_loss = float('inf')
 
 for epoch in range(N_EPOCHS):
