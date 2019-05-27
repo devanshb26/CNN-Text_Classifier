7a16c28ca5d7f9edd176ca7ae3796490a4b3bf9a
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-28 00:00:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from torchtext import datasets
 TEXT = data.Field(tokenize='spacy')
 LABEL = data.LabelField(dtype = torch.float)
 
-fields = [(None, None),('label', LABEL), ('text', TEXT)]
+fields = [(None, None), ('text', TEXT),('label', LABEL)]
 train_data, valid_data, test_data = data.TabularDataset.splits(
                                         path = '',
                                         train = 'V1.4_Training.csv',
