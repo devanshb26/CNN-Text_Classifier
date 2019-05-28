dd2353b1231c34ddf3ca7879728a5c58358683e2
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-28 15:13:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def binary_accuracy(preds, y):
     #round predictions to the closest integer
     rounded_preds = torch.round(torch.sigmoid(preds))
     correct = (rounded_preds == y).float() #convert into float for division
-    
+    print(correct)
     print(type(correct))
     print(dtype(y))
     
