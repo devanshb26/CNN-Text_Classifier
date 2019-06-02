0dcb91fc6d3624f97cdce8bad2c575b5e51897d9
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-02 22:49:57 +0530

    Update Lstm.py

--- Lstm.py
+++ Lstm.py
@@ -39,7 +39,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
     (train_data, valid_data, test_data),
-    batch_size = BATCH_SIZE, 
+    batch_size = BATCH_SIZE,
+    sort_key=lambda x: len(x.text),
     sort_within_batch = True,
     device = device)
 
