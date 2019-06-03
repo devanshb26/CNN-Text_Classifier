c3b446c9cdba2e050d290ade6672d15544a14081
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-03 14:50:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ MAX_VOCAB_SIZE = 25_000
 
 TEXT.build_vocab(train_data, 
                  max_size = MAX_VOCAB_SIZE, 
-                 vectors = 'glove.6B.100d', 
+                 vectors = 'glove.840B.300d', 
                  unk_init = torch.Tensor.normal_)
 
 LABEL.build_vocab(train_data)
@@ -102,7 +102,7 @@ class CNN(nn.Module):
         return self.fc1(cat_relu)
                  
 INPUT_DIM = len(TEXT.vocab)
-EMBEDDING_DIM = 100
+EMBEDDING_DIM = 300
 N_FILTERS = 150
 HIDDEN_DIM=250
 Dropout_2=0.75
