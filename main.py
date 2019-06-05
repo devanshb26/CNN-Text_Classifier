6971e8d3fdeeb0710b98bd19ec1f5fbc8f31135a
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-05 18:27:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ def epoch_time(start_time, end_time):
   elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
   return elapsed_mins, elapsed_secs
 
-N_EPOCHS = 100
+N_EPOCHS = 10
 best_valid_f1 = float(0)
 
 for epoch in range(N_EPOCHS):
