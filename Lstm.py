c72ce248a9c23a0df4bbda81cade67c0a34392c4
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-05 14:47:17 +0530

    Update Lstm.py

--- Lstm.py
+++ Lstm.py
@@ -113,7 +113,7 @@ OUTPUT_DIM = 1
 N_LAYERS = 2
 BIDIRECTIONAL = True
 # dropout changed from 0.5 to 0.7
-DROPOUT = 0.5
+DROPOUT = 0.6
 PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
 
 model = RNN(INPUT_DIM, 
