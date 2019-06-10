1999f5bdb5c3471b6e297d48427f38abeedd71fd
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-10 12:39:34 +0530

    Update Lstm.py

--- Lstm.py
+++ Lstm.py
@@ -119,10 +119,12 @@ class RNN(nn.Module):
                            dropout=dropout)
 #         from torch.nn import init
         for name, param in self.rnn.named_parameters():
-         print(name)
+         print(param)
          if 'bias' in name:
            nn.init.constant(param, 0.0)
+           print(0)
          elif 'weight' in name:
+           print(1)
            nn.init.kaiming_normal_(param)
 #         self.attention_layer = Attention(hidden_dim * 2,128)
 #         torch.nn.init.xavier_uniform(self.rnn.weight)
