8dd75db0e1c8501060270528fc4f572252a2a95c
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-10 12:59:18 +0530

    Update Lstm.py

--- Lstm.py
+++ Lstm.py
@@ -19,6 +19,7 @@ torch.manual_seed(SEED)
 # if torch.cuda.is_available():
 #     torch.cuda.manual_seed_all(seed)
 torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 import spacy
 nlp = spacy.load('en')
 def tokenize_en(text):
