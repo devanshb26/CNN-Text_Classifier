181ae197459edcf0a090272ebe48e5235482916c
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-10 13:08:59 +0530

    Update Lstm.py

--- Lstm.py
+++ Lstm.py
@@ -18,6 +18,7 @@ torch.manual_seed(SEED)
 # torch.manual_seed(seed)
 if torch.cuda.is_available():
   torch.cuda.manual_seed_all(SEED)
+torch.backends.cudnn.enabled = False 
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 import spacy
