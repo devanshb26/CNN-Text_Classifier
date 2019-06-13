416174a4badf3469200733df9b4a1e4ac7edcb92
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-13 14:10:40 +0530

    Update Cnn_Lstm.py

--- Cnn_Lstm.py
+++ Cnn_Lstm.py
@@ -68,7 +68,7 @@ LABEL = data.LabelField(dtype = torch.float)
 fields = [(None,None),(None,None),('text', TEXT),('label', LABEL)]
 train_data, valid_data, test_data = data.TabularDataset.splits(
                                         path = '',
-                                        train = 'V1.4_Training_downsampled.csv',
+                                        train = 'V1.4_Training.csv',
                                         validation = 'SubtaskA_Trial_Test_Labeled - Copy.csv',
                                         test = 'SubtaskA_EvaluationData_labeled.csv',
 #                                         train = 'train_spacy.csv',
