a420d79ae7b831b02cbb5e7cdb254f92719b3f18
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-15 00:53:24 +0530

    Update Lstm_basic.py

--- Lstm_basic.py
+++ Lstm_basic.py
@@ -184,13 +184,13 @@ class RNN(nn.Module):
 
       """
 
-	      hidden = final_state.squeeze(0)
-	      attn_weights = torch.bmm(lstm_output, hidden.unsqueeze(2)).squeeze(2)
-	      soft_attn_weights = F.softmax(attn_weights, 1)
-	      new_hidden_state = torch.bmm(lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)).squeeze(2)
+        hidden = final_state.squeeze(0)
+        attn_weights = torch.bmm(lstm_output, hidden.unsqueeze(2)).squeeze(2)
+        soft_attn_weights = F.softmax(attn_weights, 1)
+        new_hidden_state = torch.bmm(lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)).squeeze(2)
 
-	      return new_hidden_state
-	
+        return new_hidden_state
+
     def forward(self, text, text_lengths):
         
         #text = [sent len, batch size]
